440dfcf372c449f43e278adc8e7834ebe01ba455
darkskoll <erick.lievana@gmail.com>
2021-05-11 16:03:32 -0500

    Fixed practica16:w

--- practica16/colectiva.c
+++ practica16/colectiva.c
@@ -5,16 +5,13 @@
 int numeroDeRanks;
 int orden = 10;
 
-void llenarMatriz(int matriz[orden][orden]);
 void llenarVector(int vector[orden]);
-void imprimirMatriz(int matriz[orden][orden]);
 void imprimirVector(int vector[orden]);
 void copiarRenglonesPorRank(int rank,int origen[orden],int destino[orden]);
-void multiplicacionPorRank(int rank,int resultante[orden],int matriz[orden][orden],int vector[orden]);
+void sumaVectorPorRank(int rank,int resultante[orden],int A[orden],int B[orden]);
 
 int main(int argc, char *argv[]){
-  int A[orden][orden];
-  int B[orden],C[orden];
+  int A[orden],B[orden],C[orden];
   int rank;
   int tag=1;
   int temporal[orden];
@@ -26,26 +23,26 @@ int main(int argc, char *argv[]){
   MPI_Comm_size(MPI_COMM_WORLD, &numeroDeRanks);
   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
   start = MPI_Wtime();
-  llenarMatriz(A);
+  llenarVector(A);
   llenarVector(B);
   /*Envio y recepcion de mensajes*/
   if(rank == 0){
-    multiplicacionPorRank(rank,temporal,A,B);
+    sumaVectorPorRank(rank,temporal,A,B);
     copiarRenglonesPorRank(rank,temporal,C);
     if(numeroDeRanks > 1) MPI_Send(C,orden,MPI_INT,rank+1,tag,MPI_COMM_WORLD);
   }
   if(rank == numeroDeRanks - 1){
     if(numeroDeRanks > 1){
       MPI_Recv(C,orden,MPI_INT,rank-1,tag,MPI_COMM_WORLD,&status);
-      multiplicacionPorRank(rank,temporal,A,B);
+      sumaVectorPorRank(rank,temporal,A,B);
       copiarRenglonesPorRank(rank,temporal,C);
     }
     end = MPI_Wtime();
 
-    imprimirMatriz(A);
+    imprimirVector(A);
     printf("\n");
     imprimirVector(B);
-    printf("\nMultiplication matriz por vector terminada.\n\n");
+    printf("\nSuma de vectores terminada. rank: %d\n\n",rank);
     imprimirVector(C);
     printf("\n");
     printf("Tiempo: %lf rank: %d\n",end-start,rank);
@@ -53,7 +50,7 @@ int main(int argc, char *argv[]){
   }
   if(rank != 0 && rank != numeroDeRanks - 1){
     MPI_Recv(C,orden,MPI_INT,rank-1,tag,MPI_COMM_WORLD,&status);
-    multiplicacionPorRank(rank,temporal,A,B);
+    sumaVectorPorRank(rank,temporal,A,B);
     copiarRenglonesPorRank(rank,temporal,C);
     MPI_Send(C,orden,MPI_INT,rank+1,tag,MPI_COMM_WORLD);
   }
@@ -64,28 +61,9 @@ int main(int argc, char *argv[]){
 
 
 
-void llenarMatriz(int matriz[orden][orden]){
-  for(int i = 0; i < orden; i++){
-    for(int j = 0; j < orden; j++){
-      matriz[i][j] = rand()%20;
-    }
-  }
-}
-
 void llenarVector(int vector[orden]){
   for(int j = 0; j < orden; j++){
     vector[j] = rand()%20;
-  }
-}
-
-void imprimirMatriz(int matriz[orden][orden]){
-  printf("\n");
-  for(int i = 0; i < orden; i++){
-    printf("\t|");
-    for(int j = 0; j < orden; j++){
-      printf("%5d|",matriz[i][j]);
-    }
-    printf("\n");
   }
 }
 
@@ -104,17 +82,14 @@ void copiarRenglonesPorRank(int rank,int origen[orden],int destino[orden]){
   }
 }
 
-void multiplicacionPorRank(int rank,int resultante[orden],int matriz[orden][orden],int vector[orden]){
+void sumaVectorPorRank(int rank,int resultante[orden],int A[orden],int B[orden]){
   int from = (rank * orden)/numeroDeRanks;
   int to = ((rank+1) * orden)/numeroDeRanks;
-  int i,j;
+  int i;
 
   printf("computing rank %d (from row %d to %d)\n", rank, from, to-1);
   for (i = from; i < to; i++){
-    resultante[i] = 0;
-    for (j = 0; j < orden; j++) {
-      resultante[i] = matriz[i][j]*vector[j];
-    }
+      resultante[i] = A[i]+B[i];
   }
   printf("finished rank %d\n", rank);
 }
